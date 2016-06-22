3492bab4491f2d3d56d89fbe98b43f760a425ff2
kalsky <yanivka@gmail.com>
2016-06-21 17:55:31 -0700

    print message before the delay

--- Drivers/Shells/vCD/vcd_drop_db.py
+++ Drivers/Shells/vCD/vcd_drop_db.py
@@ -3,6 +3,7 @@
 import os
 import time
 import subprocess
+import sys
 
 with open(r'c:\ProgramData\QualiSystems\Shells.log', 'a') as f:
     f.write(time.strftime('%Y-%m-%d %H:%M:%S') + ': ' + __file__.split('\\')[-1].replace('.py', '') + ': ' + str(os.environ) + '\r\n')
@@ -22,7 +23,12 @@ with open(r'c:\deploy\drop_sql_sb.sql', 'w') as file_:
     file_.write(sqlscript)
 
 db_instance = ".\qualisystems2008"
-subprocess.check_output('sqlcmd -S ' + db_instance + ' -i c:\\deploy\\drop_sql_sb.sql')
-print 'vcloud db dropped'
+out = subprocess.check_output('sqlcmd -S .\\qualisystems2008 -i c:\\deploy\\drop_sql_sb.sql', stderr=subprocess.STDOUT)
+if 'Cannot drop database' in out:
+    print out
+    sys.exit(1)
+else:
+    print 'vcloud db dropped'
+
 
 
