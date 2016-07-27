3e25684aeaea394b3ca3c06d4ca145a5091faaee
Omri Avraham <omritk@gmail.com>
2016-07-27 13:18:22 +0300

    first onrack python commit- get_api_token

---
+++ Drivers/Shells/OnRack-Python/onrack.py
@@ -0,0 +1,98 @@
+import time
+import json
+import re
+from distutils.version import LooseVersion
+from quali_remote import rest_api_query
+from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
+from cloudshell.shell.core.context import InitCommandContext, ResourceCommandContext
+from cloudshell.api.cloudshell_api import CloudShellAPISession as cs_api
+
+
+class OnRack (ResourceDriverInterface):
+
+    def populate_resource(self, context):
+        """
+        :param ResourceCommandContext context: the context the command runs on
+        """
+        self.reservationid = context.reservation.reservation_id
+        self._cs_session(context=context)
+        token = self._get_onrack_api_token(self.address, self.user, self.password)
+
+    def deploy_esxs(self, context):
+        """
+        :param ResourceCommandContext context: the context the command runs on
+        """
+
+    def _logger(self, message, path=r'c:\ProgramData\QualiSystems\OnRack.log'):
+        try:
+            time.strptime(message[:19], '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            message = time.strftime('%Y-%m-%d %H:%M:%S') + ' ' + message
+
+        if not (message.endswith('\r\n')) or not (message.endswith('\n')):
+            message += '\r\n'
+        mode = 'a'
+        with open(path, mode=mode) as f:
+            f.write(message)
+        f.close()
+
+    def cleanup(self, chan=None):
+        if chan:
+            try:
+                chan.close()
+            except:
+                pass
+            try:
+                chan.keep_this.close()
+            except:
+                pass
+
+    def __init__(self):
+        pass
+
+    def _cs_session(self, context):
+        self.cs_api = cs_api
+        self.admin_token = context.connectivity.admin_auth_token
+        self.server_address = context.connectivity.server_address
+        self.session = self.cs_api(self.server_address, token_id=self.admin_token, domain='Global')
+        self._logger("Connected to Cloudshell API")
+
+    def initialize(self, context):
+        """
+        Initialize the driver session, this function is called every-time a new instance of the driver is created
+        This is a good place to load and cache the driver configuration, initiate sessions etc.
+        :param InitCommandContext context: the context the command runs on
+        """
+        self._cs_session(context=context)
+        self.address = context.resource.address
+        self.attrs = context.resource.attributes
+        self.user = context.resource.attributes["OnRack Username"]
+        self.password = context.resource.attributes["OnRack Password"]
+        # self.password = self.session.DecryptPassword(self.encrypted).Value
+        self.name = context.resource.name
+
+    def _WriteMessage(self, message):
+        self.session.WriteMessageToReservationOutput(self.reservationid, message)
+
+    def _get_onrack_api_token(self, address, email, password):
+        data = '''
+        {"email": "''' + email + '''",
+         "password": "''' + password + '''"
+        }
+        '''
+        url = 'https://' + address + '/login'
+        try:
+            out = rest_api_query(url=url, user='', password='', method='post', body=data, is_body_json=True, return_xml=True)
+        except Exception, e:
+            self._logger("Got Error while trying to get API token: " + str(e))
+            self._WriteMessage("Got Error while trying to get API token: " + str(e))
+            raise Exception("Got Error while trying to get API token: " + str(e))
+        self._logger("Response is: " + out)
+        out = json.loads(out)
+        auth_token = out['response']['user']['authentication_token']
+        return auth_token
+
+
+
+a = OnRack()
+print a._get_onrack_api_token('10.10.111.90', 'admin', 'admin123')
