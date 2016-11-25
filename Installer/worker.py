649d886a53881d26e3fc81aafff3f6e1e8cc4d17
Eric Rosenquist <eric.r@quali.com>
2016-11-25 08:36:19 -0800

    fix site manager package, detect package import errors in installer

--- Installer/worker.py
+++ Installer/worker.py
@@ -4,12 +4,13 @@ import os
 
 password = os.environ['QSPASSWORD']
 
-for fn in [
+zips = [
     'compute-shell.zip',
     'onrack-shell.zip',
     'site_manager_shell.zip',
     'NFV Build Environment.zip'
-]:
+]
+for fn in zips:
     print "Importing %s..." % fn
     token = requests.put('http://localhost:9000/Api/Auth/Login',
                          {"username": "admin", "password": password, "domain": "Global"}).text.strip().replace('"', '')
@@ -17,3 +18,14 @@ for fn in [
     r = requests.post('http://localhost:9000/API/Package/ImportPackage', headers={"Authorization": "Basic " + token},
                       files={"file": fileobj})
     fileobj.close()
+
+import os
+
+logdir = r'c:\program files (x86)\qualisystems\cloudshell\server\packaging logs'
+logs = os.listdir(logdir)
+
+logs.sort(key=lambda x: os.stat(os.path.join(logdir, x)).st_mtime)
+
+for log in logs[-len(zips):]:
+    if 'failed' in log.lower():
+        print 'Package import failed, check %s' % os.path.join(logdir, log)
